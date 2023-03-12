2f145871fbb55aed3f8189662078d82c084c43db
bucknorris336 <buckblocker90gmail.com>
2023-03-11 20:50:32 -0500

    added info to models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "catagory_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "catagory_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: "productTag",
+});
 module.exports = {
   Product,
   Category,
